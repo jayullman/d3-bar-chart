d8223bf87667409aeb35b5e4d8866324ebe509ef
Jay Ullman <jayullman@gmail.com>
2017-02-11 12:22:56 -0800

    Visualize bar chart

--- barChart.js
+++ barChart.js
@@ -1,9 +1,12 @@
 // will hold the received json data
 var dataset = [];
 // y scale for graph - GDP
-var y;
+var yScale;
 // x scale for graph - dates
-var x;
+var xScale;
+
+var svgWidth = 800;
+var svgHeight = 500;
 
 axios.get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
     .then(function (response){
@@ -16,21 +19,43 @@ axios.get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/m
 
 
 // called when json data is retrieved
-function printData(data) {
-    console.log(data);
-    var maxGDP = d3.max(data, function(d) { return d[1]});
-    var minGDP = d3.min(data, function(d) { return d[1]});
+function printData(dataSet) {
+    console.log(dataSet);
+    var maxGDP = d3.max(dataSet, function(d) { return d[1] });
+    var minGDP = d3.min(dataSet, function(d) { return d[1] });
     console.log(maxGDP);
     console.log(minGDP);
 
-    y = d3.scaleLinear()
+    yScale = d3.scaleLinear()
         .domain([minGDP, maxGDP])
-        .range([500, 0]);
+        .range([0, svgHeight]);
 
     // TODO: parse dates
-    x = d3.scaleLinear()
-        .domain([new Date(Date.parse(data[0])), new Date(Date.parse(data[data.length-1]))]);
-    console.log(data[0]);
-    console.log(x.domain());;
-}
+    xScale = d3.scaleLinear()
+        .domain([Date.parse(dataSet[0][0]), Date.parse(dataSet[dataSet.length-1][0])]);
+    console.log(dataSet[0]);
+    d3.select('body').append('svg')
+        .attr('width', svgWidth)
+        .attr('height', svgHeight);
 
+    d3.select("svg").selectAll('rect')
+        .data(dataSet)
+        .enter()
+        .append('rect')
+        .attr('width', function(d, i) {
+            return svgWidth / (dataSet.length);
+        })
+        .attr('height', function(d, i) {
+            return yScale(d[1]);
+        })
+        .attr('x', function(d, i) {
+            return i * (svgWidth / dataSet.length);
+        })
+        .attr('y', function(d, i) {
+            return svgHeight- yScale(d[1]);
+        })
+        .attr('fill', 'green');
+        
+
+    console.log(dataSet.length);
+}
