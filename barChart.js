8246bb92c7a65fd4554b93b02ef651dee5a5b589
Jay Ullman <jayullman@gmail.com>
2017-02-11 15:31:43 -0800

    Add y axis

--- barChart.js
+++ barChart.js
@@ -8,6 +8,7 @@ var xScale;
 var svgWidth = 800;
 var svgHeight = 500;
 var svgPadding = 20;
+var svgPaddingLeft = 50;
 
 axios.get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
     .then(function (response){
@@ -29,12 +30,16 @@ function printData(dataSet) {
 
     yScale = d3.scaleLinear()
         .domain([minGDP, maxGDP])
-        .range([0, svgHeight]);
+        .range([svgHeight - svgPadding, svgPadding]);
 
     // TODO: parse dates
     xScale = d3.scaleLinear()
         .domain([Date.parse(dataSet[0][0]), Date.parse(dataSet[dataSet.length-1][0])])
-        .range([0, svgWidth]);
+        .range([svgPaddingLeft, svgWidth - svgPadding]);
+        
+
+        console.log(Date.parse(dataSet[dataSet.length-1][0]));
+        
     console.log(dataSet[0]);
     var svg = d3.select('body').append('svg')
         .attr('width', svgWidth)
@@ -48,35 +53,37 @@ function printData(dataSet) {
             return svgWidth / (dataSet.length);
         })
         .attr('height', function(d, i) {
-            return yScale(d[1]);
+            return (svgHeight - svgPadding) - yScale(d[1]);
         })
         .attr('x', function(d, i) {
-            return i * (svgWidth / dataSet.length);
+            // console.log(xScale(Date.parse(d[0])));
+            return xScale(Date.parse(d[0]));
+
+            //return i * (svgWidth / dataSet.length);
         })
         .attr('y', function(d, i) {
-            return svgHeight- yScale(d[1]);
+            return yScale(d[1]);
         })
         .attr('fill', 'green');
         
     // create x and y axis
 
-    svg.append('p').text('YOOOO~~');
 
     var xAxis = d3.axisBottom(xScale);
-    // xAxis.tickPadding(5);
+    xAxis.tickSizeOuter(0);
+    xAxis.tickFormat(d3.timeFormat('%Y'));
 
     svg.append('g')
-        .attr('transform', 'translate(0,' + (svgHeight - svgPadding) + ')')
+        .attr('transform', 'translate(2,' + (svgHeight - svgPadding) + ')')
         .call(xAxis);
 
-    // svg.append('g')
-    // .attr("transform", "translate(0," + svgHeight + ")")
-    //     .call(d3.axisBottom(xScale));
+   
+    var yAxis = d3.axisLeft(yScale);
 
-    // svg.append("text")             
-    //   .attr("transform",
-    //         "translate(" + (svgWidth/2) + " ," + 
-    //                        (svgHeight  + 20) + ")")
-    //   .style("text-anchor", "middle")
-    //   .text("Date");
+   //yAxis.tickFormat(d3.timeFormat('%Y'));
+
+    svg.append('g')
+        .attr('transform', 'translate(50,0)')
+        .call(yAxis);
+
 }
